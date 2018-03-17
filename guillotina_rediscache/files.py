39405fb25dd7de7d6f3e88da9616a67e6d5eeb5a
vangheem <vangheem@gmail.com>
2018-03-17 15:58:54 -0400

    implement start

--- guillotina_rediscache/files.py
+++ guillotina_rediscache/files.py
@@ -10,6 +10,7 @@ class RedisFileDataManager(DBDataManager):
 
     _redis = None
     _data = None
+    _loaded = False
     _ttl = 60 * 50 * 5  # 5 minutes should be plenty of time between activity
 
     async def load(self):
@@ -21,7 +22,19 @@ class RedisFileDataManager(DBDataManager):
             if not data:
                 self._data = {}
             else:
+                self._loaded = True
                 self._data = json.loads(data)
+
+    async def start(self):
+        self._data.clear()
+        if self._loaded:
+            await self.save()
+
+    async def save(self):
+        redis = await self.get_redis()
+        key = await self.get_key()
+        await redis.set(key, json.dumps(self._data, cls=GuillotinaJSONEncoder))
+        await redis.expire(key, self._ttl)
 
     async def get_redis(self):
         if self._redis is None:
@@ -37,11 +50,8 @@ class RedisFileDataManager(DBDataManager):
         )
 
     async def update(self, **kwargs):
-        redis = await self.get_redis()
-        key = await self.get_key()
         self._data.update(kwargs)
-        await redis.set(key, json.dumps(self._data, cls=GuillotinaJSONEncoder))
-        await redis.expire(key, self._ttl)
+        await self.save()
 
     async def finish(self, values=None):
         await super().finish(values=values)
